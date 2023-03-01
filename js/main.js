1f3744704eb85c63484d5f52544b3dd9577c3417
SurajLahre <107786386+SurajLahre@users.noreply.github.com>
2023-03-01 10:43:02 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+    document.getElementById('openid').style.width = '200px';
+    
+
+}
+
+function closeNav() {
+    document.getElementById('openid').style.width = '0px';
+
+
+}
